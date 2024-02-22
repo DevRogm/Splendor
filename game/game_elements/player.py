384cd7f13fc48defaaa00d23dc81cee78bf57e5a
Mariusz <devrogm@gmail.com>
2024-02-22 16:09:53 +0100

    Work on Player class

---
+++ game/game_elements/player.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Player:
+    name: str
+    points: int = 0
+    markers: dict = field(
+        default_factory=lambda: {'emerald': 0, 'sapphire': 0, 'ruby': 0, 'diamond': 0, 'onyx': 0, 'gold': 0})
+    stone_cards: dict = field(
+        default_factory=lambda: {'emerald': [], 'sapphire': [], 'ruby': [], 'diamond': [], 'onyx': [], 'gold': []})
+
+    def __post_init__(self):
+        if len(self.name) > 20:
+            raise ValueError("Too long name, max. characters is a 20")
