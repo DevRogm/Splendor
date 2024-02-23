6ca5546926040061980b436c51f253ae21dd1fb7
Mariusz <devrogm@gmail.com>
2024-02-23 15:15:56 +0100

    Add test for aristocratic and stone cards

--- tests/test_aristocratic_cards.py
+++ tests/test_aristocratic_cards.py
@@ -1,17 +1,19 @@
 import pytest
 from game.game_elements.aristocratic_cards import AristocraticCard
+from game_data.markes_and_cards_data import aristocratic_cards
 
 
 class TestAristocraticCards:
-    data = {'requirements': {'emerald': 3, 'sapphire': 3, 'diamond': 3}, 'points': 3, 'img': 'card.png'}
-
     @pytest.fixture
     def card(self):
-        card = AristocraticCard(**self.data)
+        card = AristocraticCard(**aristocratic_cards[0])
         return card
 
     def test_card_init(self, card):
         assert card
 
-    def test_card_requirements(self, card):
+    def test_card_requirements_type(self, card):
         assert type(card.requirements) is dict
+
+    def test_card_requirements_length(self, card):
+        assert len(card.requirements) >= 1
